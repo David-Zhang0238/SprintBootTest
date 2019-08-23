4e76ace2e9178ab1b3d20848957381b39f5e560d
David-Zhang0238 <xi0238@163.com>
2019-08-23 16:58:08 +0800

    有一个项目

---
+++ env-test/src/main/java/org/crazyit/cloud/MyApplication.java
@@ -0,0 +1,15 @@
+package org.crazyit.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@ComponentScan
+public class MyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyApplication.class, args);
+	}
+
+}
